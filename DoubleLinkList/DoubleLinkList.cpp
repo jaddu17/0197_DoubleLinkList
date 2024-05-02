26a5c41181b4ff7ab175923b815d732f7b12cd3f
jaddu17 <shafajaddu@gmail.com>
2024-05-02 11:12:14 +0700

    memasukkan node baru ke dalam list

--- DoubleLinkList/DoubleLinkList.cpp
+++ DoubleLinkList/DoubleLinkList.cpp
@@ -18,4 +18,13 @@ void addNode() {
 	cin >> newNode->noMhs;				// Assign value to the data field of the new node
 	cout << "\nEnter the name of the student: ";
 	cin >> newNode->name;				// Assign value to the data field of the new node
+
+	// Insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs) {	// step 2: Insert the new node
+
+		if (START != NULL && newNode->noMhs == START->noMhs) {
+			cout << "\033[31mDuplicate roll numbers not allowed\033]0m" << endl;
+			return;
+		}
+	}
 }

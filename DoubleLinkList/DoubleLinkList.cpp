3c63f108d01632b47dd51b3de707fe5c00f44326
jaddu17 <shafajaddu@gmail.com>
2024-05-03 15:25:37 +0700

    membuat void revtraverse

--- DoubleLinkList/DoubleLinkList.cpp
+++ DoubleLinkList/DoubleLinkList.cpp
@@ -154,6 +154,15 @@ void revtraverse()
 		cout << "\nList is empty" << endl;
 	else
 	{
+		cout << "\nRecord in descending order of roll number are: " << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
 
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
 	}
 }

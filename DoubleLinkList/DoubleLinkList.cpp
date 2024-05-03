a6aa159ddb4ee4964d2168d0c5f757080852a5fa
jaddu17 <shafajaddu@gmail.com>
2024-05-03 15:11:39 +0700

    menghapus node di node pertama

--- DoubleLinkList/DoubleLinkList.cpp
+++ DoubleLinkList/DoubleLinkList.cpp
@@ -101,4 +101,14 @@ void deleteNode()
 		cout << "\033[31mThe record with roll number " << rollNo << "not found\033[0m" << endl;
 		return;
 	}
+
+	// Node to be deleted is the first node
+	if (current == START)
+	{
+		START = START->next;		// step 2: update the START pointer
+		if (START != NULL)
+		{
+			START->prev = NULL;		
+		}
+	}
 }
